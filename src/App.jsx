03175dc8a55b51a07f937dbcd503dcd954671cf8
prrranay <pkd4876308@gmail.com>
2024-03-26 16:13:11 +0530

    removed navbar and fixed ui for long input

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,6 @@ function App() {
   
   return (
     <div>
-      <Navbar />
       <div className="md:container min-h-[80vh] mx-3 md:mx-auto md:w-[60%] my-5 rounded-xl p-5 bg-violet-100">
         <h1 className="font-bold text-center text-3xl">iTask - manage your all tasks at one place.</h1>
         <div className="addTodo my-5 flex flex-col gap-4">
@@ -100,9 +99,12 @@ function App() {
           {todos.length===0 && <div className="m-5">no Tasks to display</div>}
           
           {todos.map((item) => {
-           return (showFinish || !item.isCompleted) && <div key={item.id} className="todo flex justify-between my-2">
-              <input name={item.id} onChange={handleChange} type="checkbox" checked={item.isCompleted} />
-              <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
+           return (showFinish || !item.isCompleted) && 
+           <div key={item.id} className="todo flex justify-between my-2">
+              <div className="flex h-full">
+              <input name={item.id} className="mt-2" onChange={handleChange} type="checkbox" checked={item.isCompleted} />
+              </div>
+              <div className={item.isCompleted?"line-through px-2":"px-2"}>{item.todo}</div>
               <div className="btn flex h-full">
                 <button
                   onClick={(e)=>{handleEdit(e,item.id)}}
